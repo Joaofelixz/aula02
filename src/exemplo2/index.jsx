f75b3b83aa10adcc9cf23e98f61f0165c6e3b41c
Joaofelixz <159342228+Joaofelixz@users.noreply.github.com>
2024-10-11 12:17:45 -0400

    Atualizar o index.jsx

--- src/exemplo2/index.jsx
+++ src/exemplo2/index.jsx
@@ -2,23 +2,29 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [Usuario,setUsuario] = useState([]);
 
   useEffect(() => { 
 
     const buscarUsuario  = async () => {
-                    = await fetch('');
-        const dados =                ;
-        setUsuario(dados);
+        const Usuario    = await fetch('https://json');
+        const dados =  await Usuario.json();
+        setUsuario(dados); 
     }
     buscarUsuario();
-  }, //complete o código);
+  },[]); //complete o código);
 
   return (
     <>
       <h1>Usuário</h1>
       <ul>
-        {}
+        {Usuario.map(usuario =>(
+          <li key={usuario.userId}>
+            <p>{usuario.id}</p>
+            <p>{usuario.title}</p>
+            <p>{usuario.completed}</p>
+            </li>
+          ))}
       </ul>
     </>
   );
